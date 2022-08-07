cfe1f52dd49543b1cd78972fd696b0f838f0cd82
Fuzzy Lemur <foo@bar.com>
2022-08-07 18:26:31 +0300

    fetch title data

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function DetailsPage() {
   const params = useParams();
+  const [titleData, setTitleData] = useState({});
+
+  const fetchTitleData = async () => {
+    const res = await fetch(
+      'https://imdb-api.com/API/Title/k_v3ejgbqw/' + params.id
+    );
+    const data = await res.json();
+    console.log(data);
+
+    return data;
+  };
+
+  useEffect(() => {
+    const getTitleData = async () => {
+      const data = await fetchTitleData();
+      setTitleData(data);
+    };
+    getTitleData();
+  }, []);
 
   return (
     <div>
       <h1>This is the details page</h1>
-      <h1>{params.id}</h1>
+      <h1>{JSON.stringify(titleData)}</h1>
     </div>
   );
 }
