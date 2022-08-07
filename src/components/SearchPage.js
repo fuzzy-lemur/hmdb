54f0fe8f0030e03616cc4a1cb7a134c019cc67d0
Fuzzy Lemur <foo@bar.com>
2022-08-07 13:31:28 +0300

    put table data in page scope

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,39 @@
 import SearchTable from './SearchTable';
 
+const columns = [
+  {
+      name: 'Title',
+      selector: row => row.title,
+  },
+  {
+      name: 'Year',
+      selector: row => row.year,
+  },
+];
+
+const data = [
+  {
+      id: 1,
+      title: 'Beetlejuice',
+      year: '1988',
+  },
+  {
+      id: 2,
+      title: 'Ghostbusters',
+      year: '1984',
+  },
+  {
+    id: 3,
+    title: 'Foobar',
+    year: '1984',
+},
+]
+
 function SearchPage() {
   return (
     <div>
       <h3>This is the search page</h3>
-      <SearchTable />
+      <SearchTable columns={columns} data={data}/>
     </div>
   );
 }
